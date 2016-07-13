60a0de57e0dad2e9a9817d81ccb1ec8f727ad633
Oliver Nybroe <olivernybroe@gmail.com>
2016-07-13 16:12:37 +0200

    Added example config files.

---
+++ js/config.example.js
@@ -0,0 +1,6 @@
+angular.module('config', [])
+    .constant('ENDPOINT','YOUR ENDPOINT')
+    .constant('CLIENTID','YOUR CLIENT ID')
+    .constant('ACCESSKEY','YOUR ACCESS KEY')
+    .constant('SECRETKEY','YOUR SECRET KEY')
+    .constant('REGIONNAME','YOUR REGION NAME');
